6775de283344f969212d4ab7981b822f67501e32
bhagyashree-gurav <bhagyashrigurav95@gmail.com>
2025-07-08 00:04:30 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
     backend  "s3" {
-    bucket = " terraform-backend-project-1"
+    bucket = "terraform-backend-project-1"
     region = "us-east-1"
     key = "eks/terraform.tfstate"
         }
