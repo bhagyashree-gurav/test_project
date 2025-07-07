2132c7133d3c45ddbd75efafd84da790412b5e63
bhagyashree-gurav <bhagyashrigurav95@gmail.com>
2025-07-08 00:52:28 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,11 @@
 data "aws_availability_zones" "available" {}
+resource "aws_ebs_volume" "my_volume" {
+  availability_zone = "us-east-1a"
+  size              = 8
+}
+
+resource "aws_volume_attachment" "attach" {
+  device_name = "/dev/sdf"
+  volume_id   = aws_ebs_volume.my_volume.id
+  instance_id = aws_instance.my_instance.id
+}
