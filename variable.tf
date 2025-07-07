5ec4d48158bdda56ede69ac1cef5a842bf551d9b
bhagyashree-gurav <bhagyashrigurav95@gmail.com>
2025-07-07 23:50:43 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -3,7 +3,7 @@ variable "vpc_cidr_block" {
     type = string
     default = "10.0.0.0/16"
 }
-variable "availability_zone " {
+variable "availability_zone" {
     description = "my availability zone value"
     type = string
     default = "us-east-1"
@@ -12,7 +12,8 @@ variable "subnet_cidr_block" {
     description = "name of my subnet "
     type = string
     default = "10.0.0.0/24"
-}variable "env_prefix" {
+}
+variable "env_prefix" {
     description = "prefix for environment name"
     type = string
     default = "dev"
